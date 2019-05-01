4a39d775fcc309a5f1a91bbaa5e04f281eeb3a1c
sumit kumar <sumit1523@gmail.com>
2019-05-01 16:13:10 +0530

    Improving Modal with Styled Component

--- src/Modal.js
+++ src/Modal.js
@@ -9,8 +9,11 @@ export default class Modal extends Component {
      <Portal>
          { on && (
          <ModalWrapper>
-            <button onClick={toggle}>close</button>
-            <div>{children}</div>
+            <ModalCard>
+                <CloseButton><button onClick={toggle}>close</button></CloseButton>
+                <div>{children}</div>
+            </ModalCard>
+            <Background onClick={toggle}/>
          </ModalWrapper>
          )}
      </Portal>
@@ -24,5 +27,34 @@ const ModalWrapper = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    background: teal;`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    `;
 
+const  ModalCard = styled.div`
+    position: relative;
+    background: white;
+    border-radius: 5px;
+    padding: 15px;
+    min-width: 320px;
+    z-index: 10;
+    box-shadow: 2px 2px 10px rgba(0,0,0, 0.3);
+    margin-bottom: 100px;
+`;
+
+const CloseButton = styled.button`
+    position: absolute;
+    top: 0;
+    right: 0;
+`;
+
+const Background = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: black;
+    opacity: 0.5;
+`;
